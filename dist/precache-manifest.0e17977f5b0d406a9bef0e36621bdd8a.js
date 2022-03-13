6e1685b59778377748f54c6665943274b8a44f01
killcodings <kontaktid@ya.ru>
2022-03-13 22:48:48 +0300

    init: download project

---
+++ dist/precache-manifest.0e17977f5b0d406a9bef0e36621bdd8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b97591d93aa6eec1a7c4ef273f757267",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec5d6d66fa054de446de",
+    "url": "/js/app.6a00292a.js"
+  },
+  {
+    "revision": "a3873639a6223bac2cca",
+    "url": "/js/chunk-vendors.b53b4d9f.js"
+  },
+  {
+    "revision": "03b8ad800b22244878af354b0662e153",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
